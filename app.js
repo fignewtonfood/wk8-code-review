fc5118aaf44a89500cf7fa0da32c87fdd2fef414
Tim White <fignewtonfood@gmail.com>
2015-09-25 09:39:00 -0700

    add routes to app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var studentRoster = angular.module('studentRoster', ['ui.router']);
+
+studentRoster.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+        url: "",
+        templateUrl: "partials/home.html"
+    });
+
+    $stateProvider.state('students', {
+        url: "/students",
+        templateUrl: "partials/students.html",
+        controller: "StudentsCtrl"
+    });
+
+    $stateProvider.state('teacher', {
+        url:"/teacher",
+        templateUrl: "partials/teacher.html",
+        controller: 'AttendanceCtrl'
+    });
+});
