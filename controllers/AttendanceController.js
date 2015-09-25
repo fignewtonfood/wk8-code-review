eb321cddb0bb1c2db47fe395db7d179ab4e240d2
Tim White <fignewtonfood@gmail.com>
2015-09-25 10:24:34 -0700

    add sign-in and sign-out functionality to attendance controller and students partial

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,6 +1,11 @@
 studentRoster.controller('AttendanceCtrl', function AttendanceCtrl($scope, StudentsFactory){
     $scope.students = StudentsFactory.students;
+
     $scope.addStudentIsPresent = function(student) {
         student.attendance = true;
+    };
+
+    $scope.addStudentIsGone = function(student) {
+        student.attendance = false;
     };
 });
